db0687078446193ae86be348dab31b896a14db32
Damodar Banaulikar <damodarbanaulikar@gmail.com>
2025-02-20 16:10:12 +0530

    Updated files

---
+++ src/routes/user.routes.js
@@ -0,0 +1,30 @@
+import {Router} from "express"
+import {registerUser} from "../controllers/user.controller.js"
+import {loginUser} from "../controllers/user.controller.js"
+import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {logoutUser} from "../controllers/user.controller.js"
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "CoverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured route 
+
+router.route("/logout").post(verifyJWT, logoutUser)
+
+
+export default router
